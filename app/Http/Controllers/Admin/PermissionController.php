a0cf863483f08da3e942afeb5e0012e3e1c82dcb
António Camarão <camaraoacsn@gmail.com>
2023-05-26 10:04:41 +0100

    #5 Create Roles and Permissions Pages

---
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PermissionController extends Controller
+{
+    public function index()
+    {
+        return view('admin.permissions.index');
+    }
+}
